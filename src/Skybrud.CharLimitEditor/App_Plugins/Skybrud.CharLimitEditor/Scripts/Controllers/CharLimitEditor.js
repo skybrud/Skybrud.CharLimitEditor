8aae86d2568c298455be1a3305848e09d1430706
Anders Bjerner <abjerner@skybrud.dk>
2017-03-24 14:04:03 +0100

    Initial commit

---
+++ src/Skybrud.CharLimitEditor/App_Plugins/Skybrud.CharLimitEditor/Scripts/Controllers/CharLimitEditor.js
@@ -0,0 +1,35 @@
+﻿angular.module('umbraco').controller('Skybrud.CharLimitEditorController', function ($scope, localizationService) {
+    
+    // Initialize the configuration
+    var limit = parseInt($scope.model.config.limit);
+    $scope.model.config.multiline = $scope.model.config.multiline === '1' || $scope.model.config.multiline === true;
+    $scope.model.config.enforce = $scope.model.config.enforce === '1' || $scope.model.config.enforce === true;
+
+
+
+    $scope.limitChars = function () {
+        if ($scope.model.value.length > limit && $scope.model.config.enforce) {
+            $scope.info = 'You cannot write more than <strong class="negative">' + limit + '</strong> characters!';
+
+            localizationService.localize('skyCharLimit_info3', [limit]).then(function (value) {
+                $scope.info = value;
+            });
+
+            $scope.model.value = $scope.model.value.substr(0, limit);
+        } else {
+            var remaining = (limit - $scope.model.value.length);
+            if ($scope.model.value.length > limit) {
+                localizationService.localize('skyCharLimit_info2', [remaining]).then(function (value) {
+                    $scope.info = value;
+                });
+            } else {
+                localizationService.localize('skyCharLimit_info1', [remaining]).then(function (value) {
+                    $scope.info = value;
+                });
+            }
+        }
+    };
+
+    $scope.limitChars();
+
+});
---
+++ src/Skybrud.CharLimitEditor/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Skybrud.CharLimitEditor")]
+[assembly: AssemblyDescription("Char limit property editor for Umbraco 7+")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skybrud.dk")]
+[assembly: AssemblyProduct("Skybrud.CharLimitEditor")]
+[assembly: AssemblyCopyright("Copyright © 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e7249c72-cee3-4f9e-9498-43e2754db475")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
